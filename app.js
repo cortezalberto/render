b565c85fe76d8a731acf46baa6a65a74da64973d
cortezalberto <cortezalberto@gmail.com>
2022-09-01 07:32:18 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || "3000"
+
+app.get('/', (req, res) => 
+res.send('Hello Mundo!'))
+
+
+app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
